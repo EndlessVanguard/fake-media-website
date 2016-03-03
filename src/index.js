5107135fa6a09a3f8ece3910b1078e250f940164
Martin Josefsson <hello@martinjosefsson.com>
2016-03-03 21:23:22 +0100

    Add Standard js code style checker

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const app = require("express")()
+const app = require('express')()
 
 const appConfig = {
-  apiToken: "849353fggresg5e84ja"
+  apiToken: '849353fggresg5e84ja'
 }
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send(
     `<h1>Welcome to my newspaper.</h1>
 <br/>
@@ -28,26 +28,25 @@ Bitoin Company Frees us from the Terror of Advertising
       </ul>
 `
           )
-});
+})
 
-app.get("/article/:articleId", (req, res) =>{
+app.get('/article/:articleId', (req, res) => {
   const apiToken = appConfig.apiToken
   const contentId = req.params.articleId
 
-  const clientSideScript = fs.readFileSync(path.resolve(__dirname, '../../bitcoin-remunerate-web/dist/client.js'), 'utf8');
+  const clientSideScript = fs.readFileSync(path.resolve(__dirname, '../../bitcoin-remunerate-web/dist/client.js'), 'utf8')
 
   res.send(`
 <h1>Bitoin Company Frees us from the Terror of Advertising</h1>
 <p><em>12 cat pictures that will change your life forever</em></p>
 <div id=remunerate-content >
 <script>
-(${clientSideScript})("${apiToken}", "${contentId}");
+(${clientSideScript})("${apiToken}", "${contentId}")
 </script>
 </div>
 `)
 })
 
-
 app.listen(4000, () => {
-  console.log("App running on port 3000")
+  console.log('App running on port 4000')
 })
